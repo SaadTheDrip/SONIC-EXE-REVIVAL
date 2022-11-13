74a54784d1fc3a42adcbb9eab449896257915b04
cheblol <98470285+cheblol@users.noreply.github.com>
2022-11-13 18:44:57 +0600

    checkbox

--- source/CheckboxThingie.hx
+++ source/CheckboxThingie.hx
@@ -13,24 +13,29 @@ class CheckboxThingie extends FlxSprite
 	public function new(x:Float = 0, y:Float = 0, ?checked = false) {
 		super(x, y);
 
-		frames = Paths.getSparrowAtlas('checkboxanim');
-		animation.addByPrefix("unchecked", "checkbox0", 24, false);
-		animation.addByPrefix("unchecking", "checkbox anim reverse", 24, false);
-		animation.addByPrefix("checking", "checkbox anim0", 24, false);
-		animation.addByPrefix("checked", "checkbox finish", 24, false);
-
-		antialiasing = ClientPrefs.globalAntialiasing;
+		antialiasing = false;
 		setGraphicSize(Std.int(0.9 * width));
 		updateHitbox();
 
-		animationFinished(checked ? 'checking' : 'unchecking');
-		animation.finishCallback = animationFinished;
 		daValue = checked;
+		loadGraphic(Paths.image('options/${getDaString(daValue)}'));
+		
+		scale.set(3, 3);
+	}
+
+	function getDaString(value:Bool):String 
+	{
+        if (value) {
+			return 'yes';
+		}
+		else {
+			return 'no';
+		}
 	}
 
 	override function update(elapsed:Float) {
 		if (sprTracker != null) {
-			setPosition(sprTracker.x - 130 + offsetX, sprTracker.y + 30 + offsetY);
+			setPosition(sprTracker.x + 100 + offsetX, sprTracker.y);
 			if(copyAlpha) {
 				alpha = sprTracker.alpha;
 			}
@@ -39,29 +44,7 @@ class CheckboxThingie extends FlxSprite
 	}
 
 	private function set_daValue(check:Bool):Bool {
-		if(check) {
-			if(animation.curAnim.name != 'checked' && animation.curAnim.name != 'checking') {
-				animation.play('checking', true);
-				offset.set(34, 25);
-			}
-		} else if(animation.curAnim.name != 'unchecked' && animation.curAnim.name != 'unchecking') {
-			animation.play("unchecking", true);
-			offset.set(25, 28);
-		}
+		loadGraphic(Paths.image('options/${getDaString(check)}'));
 		return check;
-	}
-
-	private function animationFinished(name:String)
-	{
-		switch(name)
-		{
-			case 'checking':
-				animation.play('checked', true);
-				offset.set(3, 12);
-
-			case 'unchecking':
-				animation.play('unchecked', true);
-				offset.set(0, 2);
-		}
 	}
 }
